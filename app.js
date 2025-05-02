9a884aecb3daf144c2e4c053f0172ddb78419f42
suppKimKr <byjiwon@korea.ac.kr>
2025-05-02 20:53:32 +0900

    [test] vercel

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ server.use(function (req, res) {
     res.render('error.ejs', { status: Format.notFound().statusCode });
 });
 
-server.listen(process.env.APP_PORT, function () {
-    console.log('[%s][%s] (http) listening on port [%s]', process.env.NODE_ENV, process.env.APP_NAME, this.address().port);
-    console.log('---------------------------------------------------------------');
-});
+// server.listen(process.env.APP_PORT, function () {
+//     console.log('[%s][%s] (http) listening on port [%s]', process.env.NODE_ENV, process.env.APP_NAME, this.address().port);
+//     console.log('---------------------------------------------------------------');
+// });
 
 module.exports = server;
