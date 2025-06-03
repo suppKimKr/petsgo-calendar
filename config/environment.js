f729dd944e36d4168c391f049c00ff4f268fc8c9
suppKimKr <byjiwon@korea.ac.kr>
2025-06-03 15:45:19 +0900

    [update] environment

--- config/environment.js
+++ config/environment.js
@@ -1,16 +1,12 @@
-const _ = require('lodash');
 const bodyParser = require('body-parser');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const compression = require('compression');
-const bearerToken = require('express-bearer-token');
-const cors = require('cors');
 const requestIp = require('request-ip');
 
 process.env.NODE_ENV = !!process.env.NODE_ENV ? process.env.NODE_ENV : 'myLocalhost';
 global.config = require('config');
 
-module.exports = async (app) => {
+module.exports = (app) => {
     app.use(bodyParser.json({ limit: '50mb' }));
 
     app.use(
@@ -20,22 +16,9 @@ module.exports = async (app) => {
         })
     );
 
-    app.set('jwt-secret', config.secret);
-
     app.use(requestIp.mw());
 
-    // CORS 설정
-    app.use(cors());
-    // app.use(
-    //     cors({
-    //         origin: ['http://localhost:8080', 'http://localhost:3333'],
-    //     })
-    // );
-
-    app.use(cookieParser());
-
     app.use(compression());
-    app.use(bearerToken());
 
     app.disable('etag');
     app.disable('x-powered-by');
